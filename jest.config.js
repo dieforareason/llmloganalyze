4f38d2d9322e0bf1ae909a8b8dc0db71b0443c89
ifanhaq <haqinhanifan@gmail.com>
2025-10-26 15:44:03 +0700

    chore: update package.json to add jest for testing and coverage reporting
    feat: modify tsconfig.json to adjust rootDir and outDir, and include test directories

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+}
